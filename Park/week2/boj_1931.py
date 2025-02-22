e345d854f328d4a33259bda1cde0203d8930d2f0
HoyiTT <pjs9177@naver.com>
2025-02-22 21:03:16 +0900

    [feat] boj week 2 finish

--- Park/week2/boj_1931.py
+++ Park/week2/boj_1931.py
@@ -1,14 +1,20 @@
 n = int(input())
 
-num = list(map(int, input().split()))
+data = []
 
-num.sort()
+for _ in range(n):
+    data.append(list(map(int, input().split())))
 
-sec = num[0]
-score = num[0]
+# 회의가 끝나는 시간 순으로 오름차순 정렬, 2순위로 회의가 시작하는 순서로 정렬
+data.sort(key = lambda x : (x[1], x[0]))
 
-for i in range(1,len(num)):
-    sec += num[i]
-    score += sec
+curEnd = data[0][1]
+count = 1
 
-print(score)
+# 만약 다음 회의 시작시간이 현재 회의 끝나는 시간과 같거나 더 늦는다면 
+for i in range(1, len(data)):
+    if data[i][0] >= curEnd:
+        count += 1
+        curEnd = data[i][1]
+
+print(count)
